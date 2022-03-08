110e1c532b10038cc64dfdb51d0a7cc6d54fe23f
Rhuan Carlos <rhuancsg@gmail.com>
2022-03-08 16:03:16 -0300

    Update Category

--- backend/src/main/java/rhuancarlos/catalog/services/CategoryService.java
+++ backend/src/main/java/rhuancarlos/catalog/services/CategoryService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import rhuancarlos.catalog.dto.CategoryDTO;
 import rhuancarlos.catalog.entities.Category;
 import rhuancarlos.catalog.repositories.CategoryRepository;
-import rhuancarlos.catalog.services.exception.EntityNotFoundException;
+import rhuancarlos.catalog.services.exception.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -29,7 +31,7 @@ public class CategoryService {
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
-		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new CategoryDTO(entity);
 	}
 
@@ -40,6 +42,20 @@ public class CategoryService {
 		entity = repository.save(entity);
 		
 		return new CategoryDTO(entity);
+	}
+
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		try {
+			Category entity = repository.getOne(id);
+			entity.setName(dto.getName());
+			entity = repository.save(entity);
+			return new CategoryDTO (entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException("id not found " + id);
+		}
+		
 	}
 	
 	
