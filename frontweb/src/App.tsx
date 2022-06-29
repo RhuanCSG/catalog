1d5ed29915841f5552e2d138452a864d3ba2f84f
Rhuan Carlos <rhuancsg@gmail.com>
2022-06-29 20:46:15 -0300

    Home styling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,13 +1,11 @@
 import Navbar from 'components/Navbar';
 import './assets/styles/custom.scss';
 import './App.css';
+import Home from 'pages/Home';
 
 const App = () => {
   return (
-    <>
-      <Navbar />
-      <h1>Hello catalog</h1>
-    </>
+    <Home/>
   );
 };
 
