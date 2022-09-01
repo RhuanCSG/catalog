defebc286e821efb4bc6bdf180da5c32c584ddbd
Rhuan Carlos <rhuancsg@gmail.com>
2022-09-01 19:56:07 -0300

    Delete product, observer pattern

--- frontweb/src/pages/Admin/Products/List/index.tsx
+++ frontweb/src/pages/Admin/Products/List/index.tsx
@@ -9,27 +9,26 @@ import { requestBackend } from 'util/requests';
 import './styles.css';
 
 const List = () => {
-
   const [page, setPage] = useState<SpringPage<Product>>();
 
+  useEffect(() => {
+    getProducts();
+  }, []);
 
+  const getProducts = () => {
+    const config: AxiosRequestConfig = {
+      method: 'GET',
+      url: '/products',
+      params: {
+        page: 0,
+        size: 50,
+      },
+    };
 
-useEffect(() => {
-  const config: AxiosRequestConfig = {
-    method: 'GET',
-    url: "/products",
-    params: {
-      page: 0,
-      size: 50,
-    }
-  };
-
-  
-  requestBackend(config)
-    .then((response) => {
+    requestBackend(config).then((response) => {
       setPage(response.data);
     });
-}, []);
+  };
 
   return (
     <div className="product-crud-container">
@@ -43,13 +42,11 @@ useEffect(() => {
         <div className="base-card product-filter-container">Search bar</div>
       </div>
       <div className="row">
-        {page?.content.map(product => (
+        {page?.content.map((product) => (
           <div key={product.id} className="col-sm-6 col-md-12">
-          <ProductCrudCard product={product} />
-        </div>
-        )) }
-
-
+            <ProductCrudCard product={product} onDelete={() => getProducts()} />
+          </div>
+        ))}
       </div>
     </div>
   );
